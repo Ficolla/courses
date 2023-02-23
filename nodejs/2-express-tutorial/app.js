d1468a948cb8c3d7e5a77d00530b544e16dd6ff3
Ficolla <n.fabiani90@gmail.com>
2023-02-23 11:35:02 +0100

    HTTP Request object

--- nodejs/2-express-tutorial/app.js
+++ nodejs/2-express-tutorial/app.js
@@ -1,1 +1,21 @@
-console.log('test');
+const http = require('http')
+const server = http.createServer((req, res) => {
+    const url = req.url;
+
+    if (url === '/') {
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write('<h1>homepage</h1>')
+        res.end()
+    } else if (url === '/about') {
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write('<h1>about</h1>')
+        res.end()
+    } else {
+        res.writeHead(400, {'content-type': 'text/html'})
+        res.write('<h1>page not found</h1>')
+        res.end()
+    }
+
+})
+
+server.listen(5000)

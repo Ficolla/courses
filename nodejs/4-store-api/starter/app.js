2f3886da29bc6f6e2872afb8082bc66998562e8e
Ficolla <n.fabiani90@gmail.com>
2023-03-18 12:15:09 +0100

    Store api project - DB connect

---
+++ nodejs/4-store-api/starter/app.js
@@ -0,0 +1,36 @@
+
+require('dotenv').config()
+// async errors
+
+const express = require('express')
+const app = express()
+
+const connectDB = require('./db/connect')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1>< href="/api/v1/products">products route</a>')
+})
+
+// products route
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}`))
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+start()

a8d544d6c18db2dde0369988f2620ad431546d8b
Ficolla <n.fabiani90@gmail.com>
2023-03-22 17:31:26 +0100

    model

--- nodejs/4-store-api/starter/middleware/error-handler.js
+++ nodejs/4-store-api/starter/middleware/error-handler.js
@@ -1,5 +1,7 @@
 const errorHandlerMiddleware = async(err, req, res, next) => {
-    return res.status(err.status).json({msg: err.message})
+    console.log(err)
+    // return res.status(err.status).json({msg: err.message})
+    return res.status(500).json({msg: 'Something went wrong'})
 }
 
 module.exports = errorHandlerMiddleware
